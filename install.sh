bd1718d5469f93328af160f147851264cb7b36cb
just-luk <lukegriffith@gmail.com>
2023-05-10 14:11:33 -0400

    update dev container

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt -y update
-sudo apt -y install graphviz graphviz-dev ffmpeg google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt -y install graphviz graphviz-dev ffmpeg google-cloud-sdk-gke-gcloud-auth-plugin kubectl
 pip install pygraphviz 
 pip install -r requirements.txt
 git clone https://github.com/CTFg/ctfg competition_infra/ctfg
-
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
