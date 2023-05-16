4a39d4f0b0274e710c7605bb085dccbd95d4d506
Luke Griffith <81766276+just-luk@users.noreply.github.com>
2023-05-16 13:44:50 +0000

    update devcontainer

--- scripts/install_codespaces.sh
+++ scripts/install_codespaces.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 git clone https://github.com/CTFg/ctfg competition_infra/ctfg
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+rm minikube_latest_amd64.deb
+
