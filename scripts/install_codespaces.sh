79f486a4d79be5a4436a9b16a3c4a39b553bb58e
Luke Griffith <81766276+just-luk@users.noreply.github.com>
2023-11-28 14:43:36 -0500

    Update install_codespaces.sh

--- scripts/install_codespaces.sh
+++ scripts/install_codespaces.sh
@@ -1,6 +1,7 @@
 sudo apt -y update
 sudo apt -y install graphviz graphviz-dev ffmpeg google-cloud-sdk-gke-gcloud-auth-plugin kubectl
 pip install -r requirements.txt
+git clone https://github.com/xctf-io/xctf competition_infra/xctf
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 rm minikube_latest_amd64.deb
